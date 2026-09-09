1f83bf10d08fc86c6c1be42f9fa3185b83c04473
Pedro Lima <pedrolima.ep@gmail.com>
2026-09-09 21:01:33 +0200

    WIP: ex01.

---
+++ ex01/Span.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/09 20:10:32 by pecavalc          #+#    #+#             */
+/*   Updated: 2026/09/09 21:01:02 by pecavalc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+# include <vector>
+
+class Span {
+  public:
+    Span(unsigned int N);
+    Span(const Span& other);
+    Span& operator=(const Span& other);
+    ~Span();
+
+    void addNumber(int number);
+    int shortestSpan();
+    int longestSpan();
+
+  private:
+    Span();
+    unsigned int N_;
+    std::vector<int> integers;
+};
+
+#endif
