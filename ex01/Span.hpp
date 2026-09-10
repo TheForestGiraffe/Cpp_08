1361fb788338e0bdfdbbfdf3323e6425a9a02ed6
pecavalc <pecavalc@student.42berlin.de>
2026-09-10 16:40:39 +0200

    Implement addMultiple function template

--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -6,14 +6,17 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/09 20:10:32 by pecavalc          #+#    #+#             */
-/*   Updated: 2026/09/10 11:54:56 by pecavalc         ###   ########.fr       */
+/*   Updated: 2026/09/10 15:57:10 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_HPP
 # define SPAN_HPP
 
+# include <cstddef>
 # include <vector>
+# include <iterator>
+# include <stdexcept>
 
 class Span {
   public:
@@ -25,6 +28,15 @@ class Span {
     void addNumber(int number);
     unsigned int shortestSpan() const;
     unsigned int longestSpan() const;
+
+    template <typename Iterator>
+    void addMultiple(Iterator first, Iterator last) {
+      std::size_t distance = static_cast<std::size_t>(std::distance(first, last));
+      if (distance > (max_size - integers.size())) {
+        throw std::runtime_error("Span > addMultiple: maximum size exceeded.");
+      }
+      integers.insert(integers.end(), first, last);
+    }
 
   private:
     Span();
