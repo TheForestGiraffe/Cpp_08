7f0fb0086a52773c5b9fbe7cfe18454d0c691ec5
pecavalc <pecavalc@student.42berlin.de>
2026-09-10 11:09:12 +0200

    Add preliminary test main

---
+++ ex01/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/10 10:56:20 by pecavalc          #+#    #+#             */
+/*   Updated: 2026/09/10 11:04:50 by pecavalc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Span.hpp"
+#include <iostream>
+
+int main() {
+  Span sp = Span(5);
+  
+  sp.addNumber(6);
+  sp.addNumber(3);
+  sp.addNumber(17);
+  sp.addNumber(9);
+  sp.addNumber(11);
+  sp.addNumber(12);
+  
+  //std::cout << sp.shortestSpan() << std::endl;
+  //std::cout << sp.longestSpan() << std::endl;
+  
+  return 0;
+}
