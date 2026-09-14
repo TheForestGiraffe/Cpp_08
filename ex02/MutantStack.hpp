4fa6f5a210efdfefdb8c8e811ecd866f77a22f3b
pecavalc <pecavalc@student.42berlin.de>
2026-09-14 12:07:52 +0200

    Fix missing colon symbol

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/10 16:55:08 by pecavalc          #+#    #+#             */
-/*   Updated: 2026/09/14 11:13:29 by pecavalc         ###   ########.fr       */
+/*   Updated: 2026/09/14 11:56:38 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <stack>
 
 template <typename T>
-class MutantStack : public std:stack<T> {
+class MutantStack : public std::stack<T> {
   public:
     typedef typename std::stack<T>::container_type::iterator iterator;
 
